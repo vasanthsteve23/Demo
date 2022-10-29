92ce581753df756293176a772520eb7a577d1955
ulfathFarhan <ulfath.farhan@gmail.com>
2022-10-29 18:08:16 +0530

    Ulfath's 1-Commit

--- Sample.java
+++ Sample.java
@@ -4,7 +4,7 @@ class Sample{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.print(a+"Hello world");
+        System.out.print(a+"Hello Da Mapla ,Kochi la Irundhu !!!");
         sc.close();
     }
 }
