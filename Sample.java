ff49f9b7f6c2b77ee216931e78170614674baec5
Vasanth Steve <vasanthsteve23@gmail.com>
2022-10-29 17:40:42 +0530

    First commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Sample{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(a+"Hello world");
+        sc.close();
+    }
+}
