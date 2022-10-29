b42b22cb58bd2556b8df972a2460e712ea6e75aa
ulfathFarhan <ulfath.farhan@gmail.com>
2022-10-29 19:11:19 +0530

    Ulfath's Third Commit

--- Sample2.java
+++ Sample2.java
@@ -5,6 +5,9 @@ public class Sample2 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.print(a + "Hello Universe...123");
+        System.out.print(a + "Hello Universe...1234");
+        System.out.print(a + "Hello Universe...12345");
+
         sc.close();
     }
 }
